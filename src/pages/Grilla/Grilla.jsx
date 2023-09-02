0ae58f30d319966002eedb674531835e674e17c1
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-09-01 21:34:38 -0300

    Update links v10

--- src/pages/Grilla/Grilla.jsx
+++ src/pages/Grilla/Grilla.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import style from "./Grilla.module.css";
 import { ButtonHome } from "../../components/ButtonHome/ButtonHome";
 import { Control } from "../../components/Control/Control";
 
 export const Grilla = () => {
   return (
     <div className="flex bg-slate-950 w-[100vw] h-[100vh]">
-      <div className={style.logo}>
-        <h3>GRILLA</h3>
+      <div className="z-10 fixed text-center top-1 left-1 w-64 h-11 bg-[#303030] text-white">
+        <h3 className="font-bold text-xl p-1">GRILLA</h3>
       </div>
       <ButtonHome />
       <iframe
